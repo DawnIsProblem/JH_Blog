e6f5880398c311c0011a3c998ad51d36ef7f80f2
makernaru-test00 <qweqwerty12321@gmail.com>
2025-01-12 15:04:35 +0900

    feat: 사용자 리포지토리 추가

---
+++ src/main/java/com/leejeonghoon/blogproject/domain/user/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.leejeonghoon.blogproject.domain.user.repository;
+
+import com.leejeonghoon.blogproject.domain.user.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByLoginId(String loginId);
+    Optional<UserEntity> findByEmail(String email);
+    Optional<UserEntity> findByPassword(String password);
+    Optional<UserEntity> findByNickName(String nickName);
+}
