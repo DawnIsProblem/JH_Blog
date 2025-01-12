88e658874d9c2d624694e6c10b5a8434203f86fd
makernaru-test00 <qweqwerty12321@gmail.com>
2025-01-12 15:04:02 +0900

    feat: 사용자 정보 반환 Dto 추가

---
+++ src/main/java/com/leejeonghoon/blogproject/domain/user/dto/response/UserResponseDto.java
@@ -0,0 +1,17 @@
+package com.leejeonghoon.blogproject.domain.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponseDto {
+    private String loginId;
+    private String nickname;
+    private String email;
+    private String profileImg;
+}
