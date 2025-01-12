4e0d8d03c305739234d13a988b8775b46b0a5389
makernaru-test00 <qweqwerty12321@gmail.com>
2025-01-12 15:03:07 +0900

    feat: JWT 설정 코드 추가

---
+++ src/main/java/com/leejeonghoon/blogproject/domain/user/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,36 @@
+package com.leejeonghoon.blogproject.domain.user.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtTokenProvider.resolveToken(request);
+
+        if(token != null && jwtTokenProvider.validateToken(token)){
+            Boolean isBlacklisted = redisTemplate.hasKey(token);
+            if(isBlacklisted != null && isBlacklisted){
+                throw new RuntimeException("로그아웃된 토큰입니다.");
+            }
+
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/com/leejeonghoon/blogproject/domain/user/jwt/JwtTokenProvider.java
@@ -0,0 +1,77 @@
+package com.leejeonghoon.blogproject.domain.user.jwt;
+
+import com.leejeonghoon.blogproject.domain.user.dto.request.UserLoginRequestDto;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    private final String secretKeyString = "cjdmadmfhghswkgksmswkrdmsvmfhwprxm";
+    private Key secretKey;
+
+    @PostConstruct
+    protected void init() {
+        this.secretKey = Keys.hmacShaKeyFor(secretKeyString.getBytes());
+    }
+
+    public UserLoginRequestDto createToken(String loginId, long validityInMilliseconds) {
+        Claims claims = Jwts.claims().setSubject(loginId);
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if(bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public Boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody();
+        String loginId = claims.getSubject();
+
+        UserDetails userDetails = new org.springframework.security.core.userdetails.User(
+                loginId, "", java.util.Collections.emptyList()
+        );
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+
+    public long getExpiration(String token) {
+        Claims claims = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody();
+        Date expirationDate = claims.getExpiration();
+        return expirationDate.getTime() - System.currentTimeMillis();
+    }
+}
