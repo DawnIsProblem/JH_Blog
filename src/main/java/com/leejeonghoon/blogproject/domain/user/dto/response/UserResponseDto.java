bc1cf41a57d375d0c58c1b8c55830497b3543d23
makernaru-test00 <qweqwerty12321@gmail.com>
2025-01-13 03:08:33 +0900

    feat: 반환 정보에 고유 ID 번호 추가

--- src/main/java/com/leejeonghoon/blogproject/domain/user/dto/response/UserResponseDto.java
+++ src/main/java/com/leejeonghoon/blogproject/domain/user/dto/response/UserResponseDto.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class UserResponseDto {
+    private Long userId;
     private String loginId;
     private String nickname;
     private String email;
